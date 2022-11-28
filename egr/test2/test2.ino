a4ad5b3f9471aeef61323e01b9157d3d21279697
dh2di3oj2 <sanjeev.chauhan@duke.edu>
2022-11-28 11:42:28 -0500

    it works finally im stupoid

--- egr/test2/test2.ino
+++ egr/test2/test2.ino
@@ -19,14 +19,14 @@
 
 //buttons
 
-const int skipPin = X;  // the number of the pushbutton pin
+const int skipPin = 1;  // the number of the pushbutton pin
 
 int skipState = 0;  // variable for reading the pushbutton status
 
-int song = 0; //song number
+int song = 0;  //song number
 
 
-const int buttonPin = X;  // the number of the pushbutton pin
+const int buttonPin =0;  // the number of the pushbutton pin
 
 int buttonState = 0;  // variable for reading the pushbutton status
 
@@ -40,12 +40,13 @@ const char filename2[] = "2.WAV";
 
 // variable representing the Wave File
 SDWaveFile waveFile;
+SDWaveFile waveFile2;
 
 void setup() {
   // Open serial communications and wait for port to open:
-  Serial.begin(9600); //115200
+  Serial.begin(9600);  //115200
   while (!Serial) {
-    ; // wait for serial port to connect. Needed for native USB port only
+    ;  // wait for serial port to connect. Needed for native USB port only
   }
 
   // setup the SD card, depending on your shield of breakout board
@@ -64,17 +65,19 @@ void setup() {
   // check if the WaveFile is valid
   if (!waveFile) {
     Serial.println("wave file 1 is invalid!");
-    while (1); // do nothing
+    while (1)
+      ;  // do nothing
   }
   if (!waveFile2) {
     Serial.println("wave file 2 is invalid!");
-    while (1); // do nothing
+    while (1)
+      ;  // do nothing
   }
 
-  
 
 
-Serial.println("info about file 1");
+
+  Serial.println("info about file 1");
 
   // print out some info. about the wave file
   Serial.print("Bits per sample = ");
@@ -94,49 +97,48 @@ Serial.println("info about file 1");
   Serial.print(duration);
   Serial.println(" seconds");
 
-  // adjust the playback volume
-  AudioOutI2S.volume(5);
+ 
 
   // check if the I2S output can play the wave file
   if (!AudioOutI2S.canPlay(waveFile)) {
     Serial.println("unable to play wave file using I2S!");
-    while (1); // do nothing
+    while (1)
+      ;  // do nothing
   }
 
 
 
 
-// info about file 2
-Serial.println("info about file 2");
+  // info about file 2
+  Serial.println("info about file 2");
 
-// print out some info. about the wave file
+  // print out some info. about the wave file
   Serial.print("Bits per sample = ");
   Serial.println(waveFile2.bitsPerSample());
 
-  long channels = waveFile2.channels();
+   channels = waveFile2.channels();
   Serial.print("Channels = ");
   Serial.println(channels);
 
-  long sampleRate = waveFile2.sampleRate();
+   sampleRate = waveFile2.sampleRate();
   Serial.print("Sample rate = ");
   Serial.print(sampleRate);
   Serial.println(" Hz");
 
-  long duration = waveFile2.duration();
+   duration = waveFile2.duration();
   Serial.print("Duration = ");
   Serial.print(duration);
   Serial.println(" seconds");
 
-
+  // adjust the playback volume
+  AudioOutI2S.volume(100);
 
   // check if the I2S output can play the wave file
   if (!AudioOutI2S.canPlay(waveFile2)) {
     Serial.println("unable to play wave file 2 using I2S!");
-    while (1); // do nothing
+    while (1)
+      ;  // do nothing
   }
-
-
-
 
 
 }
@@ -144,35 +146,39 @@ Serial.println("info about file 2");
 
 
 
-void loop() {  
+void loop() {
 
-//code a button that switches between the two files
+  //code a button that switches between the two files
 
- skipState = digitalRead(skipPin);
-buttonState = digitalRead(buttonPin);
-
+  skipState = digitalRead(skipPin);
+  buttonState = digitalRead(buttonPin);
+  Serial.println(buttonState);
+  Serial.println(skipState);
 
   // check if the pushbutton is pressed. If it is, the buttonState is HIGH:
   if (skipState == HIGH) {
-    song++; // increment
+    song++;  // increment
     if (song == 2) {
-      song = 0; // reset
+      song = 0;  // reset
     }
-  } 
-
-if (buttonState == HIGH) {
-   
-   
-
-  // play the wave file
-  if (song == 0) {
-    AudioOutI2S.play(waveFile);
-  }
-  if (song == 1) {
-    AudioOutI2S.play(waveFile2);
   }
 
+  if (buttonState == HIGH) {
+
+
+
+    // play the wave file
+    if (song == 0) {
+      Serial.println("playing wave file 1");
+      AudioOutI2S.play(waveFile);
+      
+    }
+    if (song == 1) {
+      Serial.println("playing wave file 2");
+
+      AudioOutI2S.loop(waveFile2);
+      delay(5000);
+
+    }
   }
 }
-}
-
